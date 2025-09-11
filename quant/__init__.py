3be81a100efe6a34f218946ff8b7b9b45c1057e1
dariopompucci <156500331+dariopompucci@users.noreply.github.com>
2025-09-11 10:09:09 +0800

    Update __init__.py

--- quant/__init__.py
+++ quant/__init__.py
@@ -1,9 +1,19 @@
 from .ternarize import (
-    TernaryConfig,
-    TernaryQuantizer,
-    TernaryLinear,
-    TernaryConv2d,
-    ternarize_weight,
-    enable_quant,
-    disable_quant,
+    TernaryConfig as TernaryConfig,
+    TernaryQuantizer as TernaryQuantizer,
+    TernaryLinear as TernaryLinear,
+    TernaryConv2d as TernaryConv2d,
+    ternarize_weight as ternarize_weight,
+    enable_quant as enable_quant,
+    disable_quant as disable_quant,
 )
+
+__all__ = [
+    "TernaryConfig",
+    "TernaryQuantizer",
+    "TernaryLinear",
+    "TernaryConv2d",
+    "ternarize_weight",
+    "enable_quant",
+    "disable_quant",
+]
